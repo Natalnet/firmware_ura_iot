7014c7823b7b477b3c707fc407793917a22edc47
Orivaldo Santana Jr <orivaldo@gmail.com>
2022-07-18 16:20:28 -0300

    up

--- src/Comportamento.py
+++ src/Comportamento.py
@@ -1,76 +1,86 @@
 import time 
+FRENTE = 1 
+DIREITA = 2 
+ESQUERDA = 3 
+RE = 4 
+
 class Comportamento:
     kind = 'Comportamento'
     def __init__ (self, _motores, _sensorLinhaEsq, _sensorLinhaDir):
         self.rodas = _motores
         self.sensorLinhaEsq = _sensorLinhaEsq
         self.sensorLinhaDir = _sensorLinhaDir 
-        self.delta = 30 
-        self.potenciaMaxima = 700
+        self.tempoAcao = 200
+        self.potenciaMaxima = 800
         self.potenciaLimiar = 500
         self.deltaMov = 15
         self.deltaGiro = 5
         self.movEsquerda = 0 
         self.movDireita = 0
+        self.quantidadeNiveis = 5
+        self.deltaPotencia = int( (self.potenciaMaxima - self.potenciaLimiar) / self.quantidadeNiveis )
+        self.estadoAtual = 0
+        self.estadoAnterior = -1 
+        self.nivelPotencia = 0
+        self.tempoPausa = 1000
+
+    def obtenPotenciaPorNivel(self):
+        return self.potenciaLimiar + self.deltaPotencia * self.nivelPotencia
+
+    def incrementaNivelPotencia(self):
+        self.nivelPotencia = self.nivelPotencia + 1 
+        if self.nivelPotencia > self.quantidadeNiveis: 
+            self.nivelPotencia =  self.quantidadeNiveis
 
     def seguirLinha(self): 
-        time.sleep_ms(self.delta) 
+        msg = ''
+
+
         valorSLE = self.sensorLinhaEsq.value()
         valorSLD = self.sensorLinhaDir.value()
+        #print("E: ", valorSLE, " D: ", valorSLD)  
+        
         if ( not valorSLD and not valorSLE ): 
-            if (self.movDireita < self.potenciaLimiar):
-                self.movDireita = self.potenciaLimiar
-            if (self.movEsquerda < self.potenciaLimiar):
-                self.movEsquerda = self.potenciaLimiar
-            # iguala com a menor potencia 
-            if (self.movDireita != self.movEsquerda):
-                if (self.movDireita > self.movEsquerda):
-                    self.movDireita = self.movEsquerda  
-                else:
-                    self.movEsquerda = self.movDireita
-            if (self.movEsquerda < self.potenciaMaxima):
-                self.movEsquerda = self.movEsquerda + self.deltaMov
-            if (self.movDireita < self.potenciaMaxima):
-                self.movDireita = self.movDireita + self.deltaMov 
-            self.rodas.motores(self.movEsquerda,self.movDireita)
-            return b'%s %d %d' % ("Frente!",self.movEsquerda,self.movDireita)
+            self.estadoAnterior = self.estadoAtual
+            self.estadoAtual = FRENTE
         elif (  valorSLD and  valorSLE ):
-            if (self.movDireita > -self.potenciaLimiar):
-                self.movDireita = -self.potenciaLimiar
-            if (self.movEsquerda > -self.potenciaLimiar):
-                self.movEsquerda = -self.potenciaLimiar
-            # iguala com a menor ("maior negativametne") potencia 
-            if (self.movDireita != self.movEsquerda):
-                if (self.movDireita < self.movEsquerda):
-                    self.movDireita = self.movEsquerda  
-                else:
-                    self.movEsquerda = self.movDireita                
-            if (self.movEsquerda > -self.potenciaMaxima):
-                self.movEsquerda = self.movEsquerda - self.deltaMov
-            if (self.movDireita > -self.potenciaMaxima):
-                self.movDireita = self.movDireita - self.deltaMov 
-            self.rodas.motores(self.movEsquerda,self.movDireita)
-            return b'%s %d %d' % ("Re!",self.movEsquerda,self.movDireita)
+            self.estadoAnterior = self.estadoAtual
+            self.estadoAtual = RE
         elif ( valorSLD ):
-            if (self.movDireita > -self.potenciaLimiar):
-                self.movDireita = -self.potenciaLimiar
-            if (self.movEsquerda < self.potenciaLimiar):
-                self.movEsquerda = self.potenciaLimiar
-            if (self.movEsquerda < self.potenciaMaxima):
-                self.movEsquerda = self.movEsquerda + self.deltaGiro
-            if (self.movDireita > -self.potenciaMaxima):
-                self.movDireita = self.movDireita - self.deltaGiro
-            self.rodas.motores(self.movEsquerda,self.movDireita)
-            return b'%s %d %d' % ("Direito!",self.movEsquerda,self.movDireita)
+            self.estadoAnterior = self.estadoAtual
+            self.estadoAtual = DIREITA
         elif ( valorSLE ):
-            if (self.movEsquerda > -self.potenciaLimiar ):
-                self.movEsquerda = -self.potenciaLimiar 
-            if (self.movDireita < self.potenciaLimiar):
-                self.movDireita = self.potenciaLimiar
-            if (self.movEsquerda > -self.potenciaMaxima):
-                self.movEsquerda = self.movEsquerda - self.deltaGiro
-            if (self.movDireita < self.potenciaMaxima):
-                self.movDireita = self.movDireita + self.deltaGiro
+            self.estadoAnterior = self.estadoAtual
+            self.estadoAtual = ESQUERDA 
+        semTrocaDeEstado = False  
+        if (self.estadoAnterior == self.estadoAtual ): 
+            semTrocaDeEstado = True 
+
+        if self.estadoAtual == FRENTE and semTrocaDeEstado: 
             self.rodas.motores(self.movEsquerda,self.movDireita)
-            return b'%s %d %d' % ("Esquerdo!",self.movEsquerda,self.movDireita)
+            msg = '%s %d %d' % ("Frente!",self.movEsquerda,self.movDireita)
+        elif self.estadoAtual == RE and semTrocaDeEstado: 
+            self.rodas.motores(self.movEsquerda,self.movDireita)
+            msg =  '%s %d %d' % ("Re!",-self.movEsquerda,-self.movDireita)
+        elif self.estadoAtual == DIREITA and semTrocaDeEstado: 
+            self.rodas.motores(self.movEsquerda,-self.movDireita)
+            msg =  '%s %d %d' % ("Direito!",self.movEsquerda, - self.movDireita)
+        elif self.estadoAtual == ESQUERDA and semTrocaDeEstado: 
+            self.rodas.motores(-self.movEsquerda,self.movDireita)
+            msg =  '%s %d %d' % ("Esquerdo!",-self.movEsquerda,self.movDireita)        
+
+
+        if self.estadoAtual == self.estadoAnterior:
+            self.incrementaNivelPotencia()
+                 
+            time.sleep_ms(self.tempoAcao)
+        else: 
+            self.nivelPotencia = 0
+            self.rodas.parar() 
+            msg = "Parar!"
+            time.sleep_ms(self.tempoPausa)  
+        self.movEsquerda = self.obtenPotenciaPorNivel() 
+        self.movDireita = self.obtenPotenciaPorNivel()              
+                     
+        return msg 
             
